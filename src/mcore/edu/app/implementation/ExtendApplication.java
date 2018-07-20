ea94dfec0c46e017b93a3208e58a5fafeaeb7d13
Administrator <hoyeon1008@gmaiil.com>
2018-07-20 09:29:23 +0900

    1st commit

---
+++ src/mcore/edu/app/implementation/ExtendApplication.java
@@ -0,0 +1,9 @@
+package mcore.edu.app.implementation;
+
+import m.client.android.library.core.common.MorpheusApplication;
+
+public class ExtendApplication extends MorpheusApplication {
+	public ExtendApplication() {
+
+	}
+}
